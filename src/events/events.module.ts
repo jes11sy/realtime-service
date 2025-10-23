977832f7be355b2e43734974754760ca7361d639
jes11sy <defouz03@gmail.com>
2025-10-23 12:14:35 +0400

    213213

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
 import { AuthModule } from '../auth/auth.module';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, RedisModule],
   providers: [EventsGateway],
   exports: [EventsGateway],
 })
